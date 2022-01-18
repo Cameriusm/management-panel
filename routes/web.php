a6b46e74d774b26a72aefa5856dc19155e4f3d31
Cameriusm <56196357+Cameriusm@users.noreply.github.com>
2022-01-18 08:42:06 +0700

    Add authentication

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::group(['middleware' => ['role:worker|manager|admin']], function () {
+    // Route::get('/panel', function(){
+    //     return view('test');
+    // });
+});
