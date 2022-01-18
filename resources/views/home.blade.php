31d4e5bc41185b011de0379b086ee759eae2d512
Cameriusm <56196357+Cameriusm@users.noreply.github.com>
2022-01-18 14:12:25 +0700

    Update sidebar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,14 +7,21 @@
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
+                <div class="card-body d-flex flex-column align-items-center justify-content-center ">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    {{ __('You are logged in!') }}
+<div>
+<p>
+    You are logged in!
+</p>    
+                    {{-- {{ __('You are logged in!') }} --}}
+                </div>
+                    <a href="http://localhost/management-panel/public/panel">
+                        <br/>
+                    <button type="button" class="btn btn-secondary">Перейти к панели</button></a>
                 </div>
             </div>
         </div>
--- resources/views/panel/home/index.blade.php
+++ resources/views/panel/home/index.blade.php
@@ -4,21 +4,7 @@
 @section('content')
 
     <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0">Dashboard</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Dashboard v1</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
+
     <!-- /.content-header -->
 
     <!-- Main content -->
@@ -41,6 +27,10 @@
               <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
+          {{-- @if(Auth::user->hasRole('admin'))
+   // show content related to it
+   KKLKLKLK 
+   @endif  --}}
           <!-- ./col -->
           <div class="col-lg-3 col-6">
             <!-- small box -->
