ac44fe945e721fc344209612d7190420ead81af9
Cameriusm <56196357+Cameriusm@users.noreply.github.com>
2022-01-21 12:38:06 +0700

    Fix column name

--- resources/views/panel/home/verify.blade.php
+++ resources/views/panel/home/verify.blade.php
@@ -25,7 +25,7 @@
               <tr>
                 <th class="text-center"><strong>№</strong></th>
                 <th class="text-center"><strong>Имя</strong></th>
-                <th class="text-center"><strong>Фамилия</strong></th>
+                <th class="text-center"><strong>Почта</strong></th>
                 <th class="text-center"><strong>Дата создания</strong></th>
                 <th class="text-center"><strong>Действия</strong></th>
               </tr>
@@ -33,20 +33,12 @@
             <tbody>
               @foreach ($verifieds as $verified)
               <tr>
-                <th>{{$verified->id}}</th>
-                <th>{{$verified->name}}</th>
-                <th>{{$verified->email}}</th>
-                <th>{{$verified->created_at}}</th>
-                {{-- <th>{{$verified->role_id}}</th> --}}
-                <td class="project-actions text-right">
-                  {{-- <a class="btn btn-info btn-sm" href="#"> --}}
-                  {{-- <a class="btn btn-info btn-sm" href="{{ route('post.edit', $post['id']) }}"> --}}
-                      {{-- <i class="fas fa-pencil-alt">
-                      </i>
-                      Подтвердить
-                  </a> --}}
+                <th class="text-center">{{$verified->id}}</th>
+                <th class="text-center">{{$verified->name}}</th>
+                <th class="text-center">{{$verified->email}}</th>
+                <th class="text-center">{{$verified->created_at}}</th>
+                <td class="project-actions text-center">
                   <form action="{{ route('verify.update', $verified['id']) }}" method="POST"
-                  {{-- <form action="#" method="POST" --}}
                       style="display: inline-block">
                       @csrf
                       @method('PUT')
